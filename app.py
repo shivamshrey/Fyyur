8e5d0c2391c50f12e78c22d80c10afca9dad8d47
Shivam Shrey <31056227+shivamshrey@users.noreply.github.com>
2022-01-15 17:41:27 +0530

    Add edit venue controllers

--- app.py
+++ app.py
@@ -491,6 +491,143 @@ def delete_venue(venue_id):
           'url': url_for('venues')
       })
 
+@app.route('/venues/<int:venue_id>/edit', methods=['GET'])
+def edit_venue(venue_id):
+    # Get the existing venue from the database
+    # venue = Venue.query.filter_by(id=venue_id).one_or_none()    # Returns one, None, or exception if more than one
+    venue = Venue.query.get(venue_id)  # Returns object based on primary key, or None.  Guessing get is faster than filter_by
+    if not venue:
+        # User typed in a URL that doesn't exist, redirect home
+        return redirect(url_for('index'))
+    else:
+        # Otherwise, valid venue.  We can prepopulate the form with existing data like this:
+        form = VenueForm(obj=venue)
+
+    # Prepopulate the form with the current values.  This is only used by template rendering!
+    
+    # genres needs to be a list of genre strings for the template
+    genres = [ genre.name for genre in venue.genres ]
+    
+    venue = {
+        "id": venue_id,
+        "name": venue.name,
+        "genres": genres,
+        "address": venue.address,
+        "city": venue.city,
+        "state": venue.state,
+        # Put the dashes back into phone number
+        "phone": (venue.phone[:3] + '-' + venue.phone[3:6] + '-' + venue.phone[6:]),
+        "website": venue.website,
+        "facebook_link": venue.facebook_link,
+        "seeking_talent": venue.seeking_talent,
+        "seeking_description": venue.seeking_description,
+        "image_link": venue.image_link
+    }
+
+    # venue = {
+    #     "id": 1,
+    #     "name": "The Musical Hop",
+    #     "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
+    #     "address": "1015 Folsom Street",
+    #     "city": "San Francisco",
+    #     "state": "CA",
+    #     "phone": "123-123-1234",
+    #     "website": "https://www.themusicalhop.com",
+    #     "facebook_link": "https://www.facebook.com/TheMusicalHop",
+    #     "seeking_talent": True,
+    #     "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
+    #     "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
+    # }
+    return render_template('forms/edit_venue.html', form=form, venue=venue)
+
+
+@app.route('/venues/<int:venue_id>/edit', methods=['POST'])
+def edit_venue_submission(venue_id):
+    # Much of this code same as /venue/create view.
+    form = VenueForm()
+
+    name = form.name.data.strip()
+    city = form.city.data.strip()
+    state = form.state.data
+    address = form.address.data.strip()
+    phone = form.phone.data
+    # Normalize DB.  Strip anything from phone that isn't a number
+    phone = re.sub('\D', '', phone) # e.g. (819) 392-1234 --> 8193921234
+    genres = form.genres.data                   # ['Alternative', 'Classical', 'Country']
+    seeking_talent = True if form.seeking_talent.data == 'Yes' else False
+    seeking_description = form.seeking_description.data.strip()
+    image_link = form.image_link.data.strip()
+    website = form.website.data.strip()
+    facebook_link = form.facebook_link.data.strip()
+    
+    # Redirect back to form if errors in form validation
+    if not form.validate():
+        flash( form.errors )
+        return redirect(url_for('edit_venue_submission', venue_id=venue_id))
+
+    else:
+        error_in_update = False
+
+        # Insert form data into DB
+        try:
+            # First get the existing venue object
+            venue = Venue.query.get(venue_id)
+            # venue = Venue.query.filter_by(id=venue_id).one_or_none()
+
+            # Update fields
+            venue.name = name
+            venue.city = city
+            venue.state = state
+            venue.address = address
+            venue.phone = phone
+
+            venue.seeking_talent = seeking_talent
+            venue.seeking_description = seeking_description
+            venue.image_link = image_link
+            venue.website = website
+            venue.facebook_link = facebook_link
+
+            # First we need to clear (delete) all the existing genres off the venue otherwise it just adds them
+            
+            # For some reason this didn't work! Probably has to do with flushing/lazy, etc.
+            # for genre in venue.genres:
+            #     venue.genres.remove(genre)
+                        
+            # venue.genres.clear()  # Either of these work.
+            venue.genres = []
+            
+            # genres can't take a list of strings, it needs to be assigned to db objects
+            # genres from the form is like: ['Alternative', 'Classical', 'Country']
+            for genre in genres:
+                fetch_genre = Genre.query.filter_by(name=genre).one_or_none()  # Throws an exception if more than one returned, returns None if none
+                if fetch_genre:
+                    # if found a genre, append it to the list
+                    venue.genres.append(fetch_genre)
+
+                else:
+                    # fetch_genre was None. It's not created yet, so create it
+                    new_genre = Genre(name=genre)
+                    db.session.add(new_genre)
+                    venue.genres.append(new_genre)  # Create a new Genre item and append it
+
+            # Attempt to save everything
+            db.session.commit()
+        except Exception as e:
+            error_in_update = True
+            print(f'Exception "{e}" in edit_venue_submission()')
+            db.session.rollback()
+        finally:
+            db.session.close()
+
+        if not error_in_update:
+            # on successful db update, flash success
+            flash('Venue ' + request.form['name'] + ' was successfully updated!')
+            return redirect(url_for('show_venue', venue_id=venue_id))
+        else:
+            flash('An error occurred. Venue ' + name + ' could not be updated.')
+            print("Error in edit_venue_submission()")
+            abort(500)
+
 #  Artists
 #  ----------------------------------------------------------------
 @app.route('/artists')
@@ -628,32 +765,6 @@ def edit_artist_submission(artist_id):
   # artist record with ID <artist_id> using the new attributes
 
   return redirect(url_for('show_artist', artist_id=artist_id))
-
-@app.route('/venues/<int:venue_id>/edit', methods=['GET'])
-def edit_venue(venue_id):
-  form = VenueForm()
-  venue={
-    "id": 1,
-    "name": "The Musical Hop",
-    "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
-    "address": "1015 Folsom Street",
-    "city": "San Francisco",
-    "state": "CA",
-    "phone": "123-123-1234",
-    "website": "https://www.themusicalhop.com",
-    "facebook_link": "https://www.facebook.com/TheMusicalHop",
-    "seeking_talent": True,
-    "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
-    "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
-  }
-  # TODO: populate form with values from venue with ID <venue_id>
-  return render_template('forms/edit_venue.html', form=form, venue=venue)
-
-@app.route('/venues/<int:venue_id>/edit', methods=['POST'])
-def edit_venue_submission(venue_id):
-  # TODO: take values from the form submitted, and update existing
-  # venue record with ID <venue_id> using the new attributes
-  return redirect(url_for('show_venue', venue_id=venue_id))
 
 #  Create Artist
 #  ----------------------------------------------------------------
