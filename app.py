3682f3e0769a6cb28086ff6dcf6bcceae8d6304e
Shivam Shrey <31056227+shivamshrey@users.noreply.github.com>
2022-01-15 12:13:43 +0530

    Create tables and relations

--- app.py
+++ app.py
@@ -28,6 +28,28 @@ migrate = Migrate(app, db)
 # Models.
 #----------------------------------------------------------------------------#
 
+class Genre(db.Model):
+    __tablename__ = 'Genre'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+
+# No initial population of the Genre table here
+# (just the schema is defined) and we count on the web form validators to only show valid choices (forms.py)
+
+# Association tables for Artist to Genre (many2many) and Venue to Genre (many2many)
+# DEFINE the Genre table as the child since it normally doesn't matter which we pick, but in this case,
+# its common to both many2many relationships and we have to constrain the parents to just one backref!
+artist_genre_table = db.Table('artist_genre_table',
+    db.Column('genre_id', db.Integer, db.ForeignKey('Genre.id'), primary_key=True),
+    db.Column('artist_id', db.Integer, db.ForeignKey('Artist.id'), primary_key=True)
+)
+
+venue_genre_table = db.Table('venue_genre_table',
+    db.Column('genre_id', db.Integer, db.ForeignKey('Genre.id'), primary_key=True),
+    db.Column('venue_id', db.Integer, db.ForeignKey('Venue.id'), primary_key=True)
+)
+
 class Venue(db.Model):
     __tablename__ = 'Venue'
 
@@ -40,7 +62,22 @@ class Venue(db.Model):
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
 
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    # Here we link the associative table for the m2m relationship with genre
+    genres = db.relationship('Genre', secondary=venue_genre_table, backref=db.backref('venues'))
+    # secondary links this to the associative (m2m) table name
+    # can references like venue.genres with the above statement
+    # backref creates an attribute on Venue objects so we can also reference like: genre.venues
+
+    website = db.Column(db.String(120))
+    seeking_talent = db.Column(db.Boolean, default=False)
+    seeking_description = db.Column(db.String(120))
+
+    # Venue is the parent (one-to-many) of a Show (Artist is also a foreign key, in def. of Show)
+    # In the parent is where we put the db.relationship in SQLAlchemy
+    shows = db.relationship('Show', backref='venue', lazy=True)    # Can reference show.venue (as well as venue.shows)
+
+    def __repr__(self):
+        return f'<Venue {self.id} {self.name}>'
 
 class Artist(db.Model):
     __tablename__ = 'Artist'
@@ -50,13 +87,40 @@ class Artist(db.Model):
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
+    # Genre should be its own table, with a many2many relationship with Artist
+    # and another many2many relationship with Venue
+    # genres = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
 
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    # Here we link the associative table for the m2m relationship with genre
+    genres = db.relationship('Genre', secondary=artist_genre_table, backref=db.backref('artists'))
+    # secondary links this to the associative (m2m) table name
+    # can references like artist.genres with the above statement
+    # backref creates an attribute on Artist objects so we can also reference like: genre.artists
 
-# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+    website = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean, default=False)
+    seeking_description = db.Column(db.String(120))
+
+    # Artist is the parent (one-to-many) of a Show (Venue is also a foreign key, in def. of Show)
+    # In the parent is where we put the db.relationship in SQLAlchemy
+    shows = db.relationship('Show', backref='artist', lazy=True)    # Can reference show.artist (as well as artist.shows)
+
+    def __repr__(self):
+        return f'<Artist {self.id} {self.name}>'
+
+class Show(db.Model):
+    __tablename__ = 'Show'
+  
+    id = db.Column(db.Integer, primary_key=True)
+    start_time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)  # required field
+    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False) # Foreign key is tablename.pk
+    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
+
+    def __repr__(self):
+        return f'<Show {self.id} {self.start_time} artist_id={self.artist_id} venue_id={self.venue_id}>'
+
 
 #----------------------------------------------------------------------------#
 # Filters.
